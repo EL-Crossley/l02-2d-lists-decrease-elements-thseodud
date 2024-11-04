59893695aadeb26c24a62620e8ccbdd1f03c4c68
thseodud <sonda13578@gmail.com>
2024-11-04 13:19:35 -0500

    Forgot to submit this, it was saved on my computer but not github

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 # Put your function here
-
+def decreaseElements(nums):
+    for i in range(len(nums)):
+            for j in range(len(nums[i])):
+                print(nums[i][j]-1, end = " ")
 
 # testing
 nums = [[96, 5, 23, 16, 45, 63],[20,106,50,27,38,15]]
-print(decreaseElements(nums))
+decreaseElements(nums)
